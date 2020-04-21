cb0a19c063baa8a5357f1744713473637db19aed
Drake Wilcox <drakewilcox@gmail.com>
2020-04-21 14:09:02 -0700

    add readme preview, still working on clickhandle
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: jeremy kale padot <jeremy@padot.us>

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+## Component structure:
+![component structure](/FarmersMarketDiagram.png)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/components/ScheduleControl.js
+++ src/components/ScheduleControl.js
@@ -44,9 +44,36 @@ const masterScheduleList = [
 ];
 
 class ScheduleControl extends React.Component {
-  
-}
+  constructor(props) {
+    super(props);
+    this.state = marketSchedule[0];
+  }
 
+  handleClick = (index) => {
+    this.setState(marketSchedule[index]);
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <h4>DAYS</h4>
+        <p><button onClick={() => this.handleClick(1)}>Monday</button></p>
+        <p><button onClick={() => this.handleClick(2)}>Tuesday</button></p>
+        <p><button onClick={() => this.handleClick(3)}>Wednesday My Dude</button></p>
+        <p><button onClick={() => this.handleClick(4)}>Thursday</button></p>
+        <p><button onClick={() => this.handleClick(5)}>Friday</button></p>
+        <p><button onClick={() => this.handleClick(6)}>Saturday</button></p>
+        <p><button onClick={() => this.handleClick(7)}>Sunday</button></p>
+        <Schedule
+          day={this.state.day}
+          hours={this.state.hours}
+          location={this.state.location}
+          booth={this.state.booth}
+          />
+      </React.Fragment>
+    )
+  }
+}
 
 
 
